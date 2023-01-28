594ddcd015ff218a75edaa4f68d6da150d88f831
begindeveloper <107010469+begindeveloper@users.noreply.github.com>
2023-01-28 14:04:42 -0600

    update

--- javascript projects/anima-scroll/app.js
+++ javascript projects/anima-scroll/app.js
@@ -1,2 +1,12 @@
-
+const observer = new Intersection IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        } else {
+            entry.target.classList.remove('show');
+        }
+    });
+});
 const hiddenElement = document.querySelectorAll('.hidden');
+hiddenElements.forEach((element) => observer.observe(element));
