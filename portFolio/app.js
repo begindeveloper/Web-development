0f3dfa2cbd7f25969829aab485c84bd985ab90ee
begindeveloper <107010469+begindeveloper@users.noreply.github.com>
2023-02-06 15:53:32 -0600

    update with js

--- portFolio/app.js
+++ portFolio/app.js
@@ -1,2 +1,8 @@
-const hamburgerButton = document.getElementbyId('hamburger')
-const navList = document.getElementById('navList')
+const hamburgerButton = document.getElementbyId('hamburger');
+const navList = document.getElementById('nav-List');
+
+function toggleButton() {
+    navList.classList.toggle('show')
+}
+
+hamburgerButton.addEventListener('click', toggleButton)
